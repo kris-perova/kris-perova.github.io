1c1d346bfcefb47e74b3afeb2c6e8c5ce5a09b24
kristina.perova <chris.perowa@gmail.com>
2024-07-28 13:31:38 +0200

    Added link to GitHub repository

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const projects = [
         {
             name: 'Quote machine',
-            url: 'https://kris-perova.github.io/quote-machine'
+            url: 'https://kris-perova.github.io/quote-machine',
+            gitHubUrl: 'https://github.com/kris-perova/quote-machine'
         },
         {
             name: 'Markdown Previewer',
@@ -14,10 +15,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
     projects.forEach(project => {
         const listItem = document.createElement('li');
+
+        const newParName = document.createElement('p');
         const link = document.createElement('a');
         link.href = project.url;
         link.textContent = project.name;
-        listItem.appendChild(link);
+        listItem.appendChild(newParName);
+        newParName.appendChild(link);
+
+        const newParGit= document.createElement('p');
+        const gitHubLink = document.createElement('a');
+        gitHubLink.href = project.gitHubUrl;
+        gitHubLink.textContent = 'Link to GitHub';
+        listItem.appendChild(newParGit);
+        newParGit.appendChild(gitHubLink);
         projectList.appendChild(listItem);
     });
 });
