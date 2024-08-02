e8ae632c9d4dd37e0d3116b42558428f7b37ce85
kristina.perova <chris.perowa@gmail.com>
2024-08-02 17:05:53 +0200

    feat: replace gitHub link to image button

--- script.js
+++ script.js
@@ -26,7 +26,15 @@ document.addEventListener('DOMContentLoaded', function() {
     projects.forEach(project => {
         const listItem = document.createElement('li');
         createListElement(listItem, project.url, project.name);
-        createListElement(listItem, project.gitHubUrl, 'Link to GitHub');
+
+        const anchorElement = document.createElement('a');
+        const buttonElement = document.createElement('img');
+        anchorElement.href = project.gitHubUrl;
+        buttonElement.src = 'github.png';
+
+        listItem.appendChild(anchorElement);
+        anchorElement.appendChild(buttonElement);
+
         projectList.appendChild(listItem);
     });
 });
